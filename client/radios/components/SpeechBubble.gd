8cde08c5a2347f8a33ba5461362f51dc1eeca575
Alex Ward <alxwrd@googlemail.com>
2024-04-23 22:02:29 +0100

    remove speech bubble float effect

--- client/radios/components/SpeechBubble.gd
+++ client/radios/components/SpeechBubble.gd
@@ -11,7 +11,6 @@ func _ready():
     get_tree().create_timer(duration / 2).timeout.connect(
         func callback():
             var tween = create_tween()
-            tween.parallel().tween_property(self, "position", get_position() + Vector2(0, -50), duration / 4)
-            tween.parallel().tween_property(self, "modulate", Color(1, 1, 1, 0), duration / 4)
+            tween.parallel().tween_property(self, "modulate", Color(1, 1, 1, 0), duration / 2)
             tween.tween_callback(self.queue_free)
     )
